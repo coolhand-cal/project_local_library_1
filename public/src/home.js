7a380d3d612235614b08d3759014c36f15dcb314
coolhand-cal <workachieveprogress@gmail.com>
2021-02-07 11:55:10 -0500

    completed qualified assesment

--- public/src/home.js
+++ public/src/home.js
@@ -27,27 +27,98 @@ function getMostCommonGenres(books) {
   //2) should limit the list to the top five
 
   //get a list of all generes and how many times it occured
-  let genres = books.reduce((topGenres,{genre}) => {
-    topGenres[genre] === undefined ? topGenres[genre] = 1 : topGenres[genre] +=1;
+  const genres = books.reduce((topGenres,{genre}) => {
+    
+      //IF THERE Is one with the genre add to it
+      if (topGenres.some((thisgenre) => {
+        if (thisgenre.name === genre) {
+          thisgenre.count += 1;
+          return true;
+        }
+  })) {
+    
+      } 
+        //else create it and set count to 1
+      else {
+        newgenre = {};
+        newgenre[`name`] = genre;
+        newgenre[`count`] = 1
+        topGenres.push(newgenre);
+      }
     
     return topGenres;
   },[]); 
-  console.log(genres);
 
-  // i dont understand why sort function is never entered
- return genres.sort((genreA, genreB) => genreA - genreB);
+
+  return cutToFive(genres.sort((genreA, genreB) => genreB.count - genreA.count));
+
+
   
-
- 
 }
 
-// helper function to sort items
-function
+
+// helper function to cut items
+function cutToFive(items){
+ while (items.length >5) {
+   items.pop();
+ }
+ return items;
+}
 
 
-function getMostPopularBooks(books) {}
+function getMostPopularBooks(books) {
 
-function getMostPopularAuthors(books, authors) {}
+    //get an array of each book and how many times it was rented
+  const result = books.map((book) => {
+    const item = {};
+    item[`name`] = book.title; 
+    item[`count`] = book.borrows.length;
+    return item;
+  });
+
+  return cutToFive(result.sort((bookA, bookB) => bookB.count - bookA.count));
+
+}
+
+
+
+function findAndAdd(authorPool, whichAuthor, howMany){
+  authorPool.map((author) => {
+    if (author.id === whichAuthor) author.count +=howMany;
+    return author;
+  });
+}
+
+function getMostPopularAuthors(books, authors) {
+
+// take authors and ma each one to an author object
+const authorPool = authors.map((author) => {
+  author[`name`] = `${author.name.first} ${author.name.last}`;
+  author[`count`] = 0;
+  return author;
+});
+
+// go through books and add each one to authorpool count
+  const topList = books.reduce((topAuthors,book) => {
+ 
+    
+    whichAuthor = book.authorId;
+    findAndAdd(topAuthors, whichAuthor,book.borrows.length);
+    return topAuthors;
+
+   
+    
+},authorPool);
+
+result = topList.map((author) => {
+  delete author.id;
+  return author;
+});
+
+
+return cutToFive(topList.sort((authorA, authorB) => authorB.count - authorA.count));
+
+}
 
 module.exports = {
   totalBooksCount,
